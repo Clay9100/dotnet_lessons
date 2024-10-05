b70ae5251d679f31213ce423e93c8c6e0a6a4016
Muhammadsobir Turdaliyev <muhammadsobirturdaliyev9199@gmail.com>
2024-10-05 11:51:33 +0500

    The is department print "Hello world"

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson_1
+{
+
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello world");
+
+        }
+    }
+}

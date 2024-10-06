e496e40cdb9d818a7d6bda374beb8b020bda9a59
Muhammadsobir Turdaliyev <muhammadsobirturdaliyev9199@gmail.com>
2024-10-06 12:03:00 +0500

    new lesson_3 print formatting

---
+++ lesson_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PrintFormatting
+{
+
+    class Formatting
+    {
+        static void Main(string[] args)
+        {
+            // print Console.Write formatting
+
+            int k = 67;
+            System.Console.WriteLine(k);
+
+            // format
+
+            System.Console.WriteLine($"Number : {k}");
+
+            System.Console.WriteLine("Number k : {0}", k);
+
+            System.Console.WriteLine("Number k : " + k);
+
+
+        }
+    }
+}

659f620c537e48657d96782fe9a0cd257c0da5cd
Muhammadsobir Turdaliyev <muhammadsobirturdaliyev9199@gmail.com>
2024-10-06 15:51:01 +0500

    add input user read console

--- User_Input/Program.cs
+++ User_Input/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace User_Inputs
+{
+
+    class UserInput
+    {
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("The is lesson User input Console");
+
+            // User input string
+
+            /* System.Console.Write("Enter your name : ");
+
+             string name = Console.ReadLine();
+
+             System.Console.WriteLine($"Your name is : {name}");
+             */
+
+            // input int parse function
+
+            /*System.Console.Write("Enter your age : ");
+
+            int age = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine($"Your age is : {age}");
+            */
+
+            /*
+            System.Console.Write("Enter your age : ");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.WriteLine($"Your age is : {age}");
+
+            */
+
+            System.Console.Write("Enter pi : ");
+
+            //float pi = float.Parse(Console.ReadLine());
+            float pi = Convert.ToSingle(Console.ReadLine());
+
+            System.Console.WriteLine($"Pi is : {pi}");
+
+
+
+
+
+
+        }
+    }
+}

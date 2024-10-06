48244aa4c1e05bb66d242ea95a1b1a8f38ffa174
Muhammadsobir Turdaliyev <muhammadsobirturdaliyev9199@gmail.com>
2024-10-06 11:16:23 +0500

    new main file create data types

---
+++ lesson_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DataTypes
+{
+
+    class MyClass
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("The is lesson2 data types");
+
+
+        }
+    }
+}

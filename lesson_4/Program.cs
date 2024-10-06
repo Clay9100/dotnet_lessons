982065b36f923a847549c4a58640abec17d4bd0e
Muhammadsobir Turdaliyev <muhammadsobirturdaliyev9199@gmail.com>
2024-10-06 14:32:11 +0500

    add lesson_4 [+, -, /, *, %]

--- lesson_4/Program.cs
+++ lesson_4/Program.cs
@@ -6,7 +6,52 @@ namespace Operators
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("The is lesson Operators info");
+            Console.WriteLine("The is lesson Operators info");
+
+            // Add +
+
+            int x = 2;
+            int y = 3;
+
+            int sum = x + y;
+
+            Console.WriteLine($"({x} + {y}) = {sum}");
+
+            // subtraction
+
+            int a = 4;
+            int b = 1;
+
+            int subtraction = a - b;
+
+            Console.WriteLine($"({a} - {b}) = {subtraction}");
+
+            // division
+
+            int q = 8;
+            int w = 5;
+
+            int division = q / w;
+
+            Console.WriteLine($"({q} / {w}) = {division}");
+
+            // multiplication
+
+            int e = 6;
+            int r = 3;
+
+            int multiplication = e * r;
+
+            Console.WriteLine($"({e} * {r}) = {multiplication}");
+
+            // residual
+
+            int t = 8;
+            int u = 7;
+
+            int residual = t % u;
+            Console.WriteLine($"({t} % {u}) = {residual}");
+
 
         }
     }
--- lesson_4/obj/Debug/net8.0/lesson_4.AssemblyInfo.cs
+++ lesson_4/obj/Debug/net8.0/lesson_4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lesson_4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79c221c806d0ad387861f6ca230499a57454643e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19b6c414bee4d55340fded15d92074eb4a7ebc78")]
 [assembly: System.Reflection.AssemblyProductAttribute("lesson_4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lesson_4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

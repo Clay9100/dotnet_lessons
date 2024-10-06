29f28298827c3068248b174cca878b72e788e1b0
Muhammadsobir Turdaliyev <muhammadsobirturdaliyev9199@gmail.com>
2024-10-06 15:15:37 +0500

    chhange lesson Variable write types in programming

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Variables
+{
+
+    class Variable
+    {
+
+        static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("The is lesson Variables");
+
+            // Variables 
+
+            int x = 2;
+            int v = 123;
+
+            System.Console.WriteLine(x);
+            System.Console.WriteLine(v);
+
+            string str = "Hello";
+            System.Console.WriteLine(str);
+
+            char character = '$';
+            System.Console.WriteLine(character);
+
+            long l = 123456;
+
+            System.Console.WriteLine(l);
+
+            dynamic dy = 2345432;
+
+            System.Console.WriteLine(dy);
+
+            decimal dc = 123.43M;
+
+            System.Console.WriteLine(dc);
+
+            double summa = 23413235.655;
+
+            System.Console.WriteLine(summa);
+
+            float f = 21243.45F;
+
+            System.Console.WriteLine(f);
+
+            byte by = 123;
+
+            System.Console.WriteLine(by);
+
+            short sh = 2345;
+
+            System.Console.WriteLine(sh);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

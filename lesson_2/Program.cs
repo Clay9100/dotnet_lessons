3560f2b61fa8f6bd7ad965ec144312486723b879
Muhammadsobir Turdaliyev <muhammadsobirturdaliyev9199@gmail.com>
2024-10-06 11:53:15 +0500

    new types pointting and dynacim

--- lesson_2/Program.cs
+++ lesson_2/Program.cs
@@ -48,6 +48,85 @@ namespace DataTypes
             nint ni = -12345;
             System.Console.WriteLine(ni);
 
+            // nuint
+            nuint nui = 12345432;
+            System.Console.WriteLine(nui);
+
+            // long 
+
+            ulong ul = 23456788765;
+            System.Console.WriteLine(ul);
+
+
+            // long 
+
+            long l = -234354532;
+            System.Console.WriteLine(l);
+
+            // string 
+
+            string s = "Hello";
+            System.Console.WriteLine(s);
+
+            // char array 
+
+            char[] arr = { 'w', 'e', 'l', 'c', 'o', 'm', 'e' };
+            System.Console.WriteLine(arr[2]);
+
+            // char 
+
+            char c = '#'
+            System.Console.WriteLine(c);
+
+            // boolean 
+
+            bool bl = true;
+            System.Console.WriteLine(bl);
+
+            // dynacims
+
+            //var 
+
+            var vr = 123454;
+            System.Console.WriteLine(vr);
+
+            // object
+
+            object obj = true;
+            obj = 12345;
+            obj = "Esye"
+            obj = true;
+            obj = 233.5;
+            System.Console.WriteLine(obj);
+
+            // dynamic
+
+            dynamic dy = 1234432.544;
+            System.Console.WriteLine(dy);
+
+            // decimal 
+
+            decimal dc = 12345432.56m;
+            System.Console.WriteLine(dc);
+
+            // pointting
+
+            // folat 
+
+            float f = 3.14f;
+            System.Console.WriteLine(f);
+
+            // double 
+            double db = 1234543.44;
+
+            System.Console.WriteLine(db);
+
+
+
+
+
+
+
 
 
         }

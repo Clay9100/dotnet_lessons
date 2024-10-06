875174e10d3dd06a32930a089afc2a7c59cc8c10
Muhammadsobir Turdaliyev <muhammadsobirturdaliyev9199@gmail.com>
2024-10-06 11:12:15 +0500

    new lesson_2 create

--- lesson_1/obj/Debug/net8.0/dotnet_lessons.AssemblyInfo.cs
+++ lesson_1/obj/Debug/net8.0/dotnet_lessons.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet_lessons")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f6f3ceffd8688974eef92b7623747bd434056b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d273041b3ada5ff57ed83b9898610a6bbb519abc")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet_lessons")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet_lessons")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
